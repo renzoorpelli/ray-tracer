82e83cdf12197c7e7db4b49d34e64bf6da6f4bb0
renzo.orpelli <orpellirenzo@gmail.com>
2025-08-02 19:47:37 -0300

    refactor

--- src/camera.cpp
+++ src/camera.cpp
@@ -8,23 +8,23 @@
 #include <iostream>
 
 // Image
-const double aspect_ratio = 16.0 / 9.0; // Rendered img widht over height.
-const int image_width = 400;            // Rendered img width in pxl count.
-const int samples_per_pixel = 100;      // COunt of random samples for each pxl.
+const double aspectRatio = 16.0 / 9.0; // Rendered img widht over height.
+const int imageWidth = 400;            // Rendered img width in pxl count.
+const int samplesPerPixel = 100;      // COunt of random samples for each pxl.
 const auto maxDepth = 50;               // Maximum number of ray bounes into the scene.
 
-int image_height = int(image_width / aspect_ratio); // Rendered image height
-double pixel_samples_scale;                         // Color scale factor for a sum of pixel samples
+int imageHeight = int(imageWidth / aspectRatio); // Rendered image height
+double pixelSamplesScale;                         // Color scale factor for a sum of pixel samples
 Point3 center;                                      // Camera center
-Point3 pixel00_loc;                                 // Location of pixel (0,0)
-Vec3 pixel_delta_u;                                 // Offset to pixel to right
-Vec3 pixel_delta_v;                                 // Offset to pixel below
+Point3 initialPixel;                                 // Location of pixel (0,0)
+Vec3 pixelDeltaU;                                 // Offset to pixel to right
+Vec3 pixelDeltaV;                                 // Offset to pixel below
 
 // Camera
-const auto focal_length = 1.0;
-const auto viewport_height = 2.0;
-const auto viewport_width =
-    viewport_height * double(image_width) / image_height;
+const auto focalLength = 1.0;
+const auto viewportHeight = 2.0;
+const auto viewportWidth =
+    viewportHeight * double(imageWidth) / imageHeight;
 
 Vec3 SampleSquare()
 {
@@ -36,31 +36,31 @@ Vec3 SampleSquare()
 void Initialize()
 {
 
-  image_height = int(image_width / aspect_ratio);
-  image_height = (image_height < 1) ? 1 : image_height;
+  imageHeight = int(imageWidth / aspectRatio);
+  imageHeight = (imageHeight < 1) ? 1 : imageHeight;
 
-  pixel_samples_scale = 1.0 / samples_per_pixel;
+  pixelSamplesScale = 1.0 / samplesPerPixel;
 
   center = Point3(0, 0, 0);
 
   // Determine viewport dimensions.
-  auto focal_length = 1.0;
-  auto viewport_height = 2.0;
-  auto viewport_width = viewport_height * (double(image_width) / image_height);
+  auto focalLength = 1.0;
+  auto viewportHeight = 2.0;
+  auto viewportWidth = viewportHeight * (double(imageWidth) / imageHeight);
 
   // Calculate the vectors across the horizontal and down the vertical viewport
   // edges.
-  auto viewport_u = Vec3(viewport_width, 0, 0);
-  auto viewport_v = Vec3(0, -viewport_height, 0);
+  auto viewportU = Vec3(viewportWidth, 0, 0);
+  auto viewportV = Vec3(0, -viewportHeight, 0);
 
   // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-  pixel_delta_u = viewport_u / image_width;
-  pixel_delta_v = viewport_v / image_height;
+  pixelDeltaU = viewportU / imageWidth;
+  pixelDeltaV = viewportV / imageHeight;
 
   // Calculate the location of the upper left pixel.
-  auto viewport_upper_left =
-      center - Vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-  pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+  auto viewportUpperLeft =
+      center - Vec3(0, 0, focalLength) - viewportU / 2 - viewportV / 2;
+  initialPixel = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
 }
 
 Ray GetRay(int i, int j)
@@ -70,13 +70,13 @@ Ray GetRay(int i, int j)
   // sampled point around the pixel location i, j.
 
   auto offset = SampleSquare();
-  auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) +
-                      ((j + offset.y()) * pixel_delta_v);
+  auto pixelSample = initialPixel + ((i + offset.x()) * pixelDeltaU) +
+                      ((j + offset.y()) * pixelDeltaV);
 
-  auto ray_origin = center;
-  auto ray_direction = pixel_sample - ray_origin;
+  auto rayOrigin = center;
+  auto rayDirection = pixelSample - rayOrigin;
 
-  return Ray(ray_origin, ray_direction);
+  return Ray(rayOrigin, rayDirection);
 }
 
 /// @brief Calculate the color seen along a ray.
@@ -100,8 +100,8 @@ Color RayColor(const Ray &r, int depth, const HittableList &world)
     return 0.1 * RayColor(Ray(rec.p, scatterDirection), depth - 1, world);
   }
 
-  Vec3 unit_direction = UnitVector(r.Direction());
-  auto a = 0.5 * (unit_direction.y() + 1.0);
+  Vec3 unitDirection = UnitVector(r.Direction());
+  auto a = 0.5 * (unitDirection.y() + 1.0);
   return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
 }
 
@@ -111,21 +111,21 @@ void Render(const HittableList &world)
 
   std::ofstream file("image.ppm");
   file << "P3\n"
-       << image_width << ' ' << image_height << "\n255\n";
+       << imageWidth << ' ' << imageHeight << "\n255\n";
 
-  for (int j = 0; j < image_height; j++)
+  for (int j = 0; j < imageHeight; j++)
   {
-    std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
+    std::clog << "\rScanlines remaining: " << (imageHeight - j) << ' '
               << std::flush;
-    for (int i = 0; i < image_width; i++)
+    for (int i = 0; i < imageWidth; i++)
     {
-      Color pixel_color(0, 0, 0);
-      for (int sample = 0; sample < samples_per_pixel; sample++)
+      Color pixelColor(0, 0, 0);
+      for (int sample = 0; sample < samplesPerPixel; sample++)
       {
         Ray r = GetRay(i, j);
-        pixel_color += RayColor(r, maxDepth, world);
+        pixelColor += RayColor(r, maxDepth, world);
       }
-      WriteColor(file, pixel_samples_scale * pixel_color);
+      WriteColor(file, pixelSamplesScale * pixelColor);
     }
   }
 
